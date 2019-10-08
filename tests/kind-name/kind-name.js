c7a7d927fc6bf411750cd08474e70eed8de26b32
Roy Sutton <roy.sutton@lge.com>
2019-10-08 17:11:27 -0400

    Initial tests with failing test
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ tests/kind-name/kind-name.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const rule = require('../../lib/rules/kind-name'),
+	RuleTester = require('eslint').RuleTester;
+
+RuleTester.setDefaultConfig({
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true
+		}
+	}
+});
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('kind-name', rule, {
+
+	valid: [
+		"const t = kind({name: 'hello'});",
+	],
+
+	invalid: [
+		{
+			code: "const t = kind({render: (props) => (<div {...props}>Hello</div>)});",
+			errors: [{
+				message: 'Component definition is missing a name property',
+				type: 'ObjectExpression'
+			}]
+		}
+	]
+});
