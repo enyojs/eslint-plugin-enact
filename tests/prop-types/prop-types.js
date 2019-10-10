0a80c0523093e60dbb3880b8e7eccd56f690df3b
Roy Sutton <roy.sutton@lge.com>
2019-10-09 20:39:12 -0400

    Add tests for prop-types
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ tests/prop-types/prop-types.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const rule = require('../../lib/rules/prop-types'),
+	RuleTester = require('eslint').RuleTester;
+
+RuleTester.setDefaultConfig({
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true
+		}
+	}
+});
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('prop-types', rule, {
+
+	valid: [
+		"kind({propTypes: {one: PropTypes.string}, render: ({one}) => (<div>{one}</div>)});",
+		"kind({propTypes: {one: PropTypes.string}, computed: {fred: ({one}) => 1}});",
+		"kind({propTypes: {one: PropTypes.string}, handlers: {fred: (ev, {one}) => 1}});",
+		"kind({propTypes: {one: PropTypes.string}, handlers: {fred: (ev, {one}) => ev.target}});",
+		"kind({propTypes: {one: PropTypes.string}, computed: {fred: ({one}) => 1}, render: ({one, fred}) => (<div>{one}</div>)});",
+		"kind({propTypes: {one: PropTypes.string}, handlers: {fred: (ev, {one}) => 1}, render: ({one, fred}) => (<div>{one}</div>)});",
+		"kind({propTypes: {one: PropTypes.string}, computed: {one: ({one}) => 1}, render: ({one}) => (<div>{one}</div>)});",
+		"kind({propTypes: {one: PropTypes.string}, handlers: {one: (ev, {one}) => 1}, render: ({one}) => (<div>{one}</div>)});",
+	],
+	invalid: [
+		{
+			code: "kind({render: ({one}) => <div>{one}</div>});",
+			errors: [{
+				message: '\'one\' is missing in props validation',
+				type: 'Property'
+			}]
+		},
+		{
+			code: "kind({render: (props) => <div>{props.one}</div>});",
+			errors: [{
+				message: '\'one\' is missing in props validation',
+				type: 'Identifier'
+			}]
+		},
+		{
+			code: "kind({computed: {fred: ({one}) => 1}});",
+			errors: [{
+				message: '\'one\' is missing in props validation',
+				type: 'Property'
+			}]
+		},
+		{
+			code: "kind({computed: {fred: (props) => (props.one)}});",
+			errors: [{
+				message: '\'one\' is missing in props validation',
+				type: 'Identifier'
+			}]
+		},
+		{
+			code: "kind({handlers: {fred: (ev, {one}) => 1}});",
+			errors: [{
+				message: '\'one\' is missing in props validation',
+				type: 'Property'
+			}]
+		},
+		{
+			code: "kind({handlers: {fred: (ev, props) => (props.one)}});",
+			errors: [{
+				message: '\'one\' is missing in props validation',
+				type: 'Identifier'
+			}]
+		},
+		{
+			code: "kind({handlers: {fred: (ev, props) => 1}, computed: {bilbo: ({fred}) => 2}});",
+			errors: [{
+				message: '\'fred\' is missing in props validation',
+				type: 'Property'
+			}]
+		},
+		{
+			code: "kind({handlers: {fred: (ev, {bilbo}) => 1}, computed: {bilbo: (props) => 2}});",
+			errors: [{
+				message: '\'bilbo\' is missing in props validation',
+				type: 'Property'
+			}]
+		},
+	]
+});
