700cbf60c73530f17024e02ac70b0bb2a98b9b75
Daniel Stoian <daniel.stoian@lgepartner.com>
2024-11-29 13:03:11 +0200

    fixed lint errors

--- tests/display-name/display-name.js
+++ tests/display-name/display-name.js
@@ -25,31 +25,32 @@ ruleTester.run('display-name', rule, {
 			ignoreTranspilerName: true
 		}]
 	},
-		{
-			code: "const t = kind({computed: { myProp: (props) => (<div {...props}>Hello</div>)}});",
+	{
+		code: "const t = kind({computed: { myProp: (props) => (<div {...props}>Hello</div>)}});",
+		options: [{
+			ignoreTranspilerName: true
+		}]
+	},
+	{
+		code: "const t = kind({handlers: { myProp: (props) => (<div {...props}>Hello</div>)}});",
+		options: [{
+			ignoreTranspilerName: true
+		}]
+	/*	},{  Disabling this one as this triggers errors upstream, too.
+			code: "const t = kind({handlers: { myProp: (props) => ({another}) => (<div {...props}>Hello</div>)}});",
 			options: [{
 				ignoreTranspilerName: true
 			}]
-		},{
-			code: "const t = kind({handlers: { myProp: (props) => (<div {...props}>Hello</div>)}});",
-			options: [{
-				ignoreTranspilerName: true
-			}]
-			/*	},{  Disabling this one as this triggers errors upstream, too.
-					code: "const t = kind({handlers: { myProp: (props) => ({another}) => (<div {...props}>Hello</div>)}});",
-					options: [{
-						ignoreTranspilerName: true
-					}]
-			*/
-		}],
+	*/
+	}],
 
 	invalid: [
 		{
 			code: `
 			class Hello extends React.Component {
-			  render() {
-				return <div>Hello {this.props.name}</div>;
-			  }
+				render() {
+					return <div>Hello {this.props.name}</div>;
+				}
 			}
 		  `,
 			options: [{
